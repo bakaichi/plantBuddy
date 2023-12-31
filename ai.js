6dfec07d209964464c2a368f0a3f7d9fee929e78
Aidas <vaiciunasaidas@gmail.com>
2023-12-31 18:40:46 +0000

    openai api call updated to newer version + displaying the ai comment on html

--- ai.js
+++ ai.js
@@ -1,19 +1,22 @@
 import fetch from 'node-fetch';
+import 'dotenv/config';
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const OPENAI_API_URL = "https://api.openai.com/v1/engines/davinci/completions";
+const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
 
-// generate a comment based on the plant condition
 async function generateComment(temp, humidity, moisture) {
-    const prompt = `Given the following sensor readings: Temperature: ${temp}°C, Humidity: ${humidity}%, Moisture: ${moisture} - provide a brief witty comment on the plants condition.`;
+    let prompt = `Create a fun comment about a houseplant given these conditions, round numbers to nearest decimal point. Make sure to complete sentences fully 
+    before sending response: Temperature is ${temp}°C, Humidity is ${humidity}%, Soil Moisture is ${moisture}%.`;
+    console.log("Generated Prompt:", prompt);
 
     const data = {
-        prompt: prompt,
-        max_tokens: 60,
-        temperature: 0.5,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0
+        model: "gpt-3.5-turbo",  
+        messages: [{
+            role: "system",
+            content: prompt
+        }],
+        max_tokens: 40,
+        temperature: 0.7
     };
 
     const headers = {
@@ -27,16 +30,18 @@ async function generateComment(temp, humidity, moisture) {
             headers: headers,
             body: JSON.stringify(data)
         });
-        
+
         const result = await response.json();
-        if (result.choices && result.choices.length > 0) {
-            return result.choices[0].text.trim();
+        console.log("OpenAI Response:", result);
+
+        if (result.choices && result.choices.length > 0 && result.choices[0].message) {
+            return result.choices[0].message.content.trim();
         } else {
             console.log("No choices available in response.");
             return '';
         }
     } catch (error) {
-        console.error("Error calling OpenAi Api:", error);
+        console.error("Error calling OpenAI API:", error);
         return '';
     }
 }
